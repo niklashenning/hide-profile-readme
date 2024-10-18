0719c49472b1fe221e36b77892ae22a6a77c5cac
Niklas Henning <niklashenningbusiness@gmail.com>
2024-10-18 10:08:53 +0200

    Add comments

--- src/js/content.js
+++ src/js/content.js
@@ -3,12 +3,14 @@ let profileReadmeFound = false;
 
 
 window.onload = function() {
+    // Check if profile README exists on this page
     let userProfileFrame = document.getElementById("user-profile-frame");
 
     if (userProfileFrame && userProfileFrame.getElementsByClassName("Box mt-4")[0]) {
         profileReadmeFound = true;
     }
 
+    // Add listenter for status request by the popup script
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
             if (request.message === "status") {
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,12 +1,14 @@
 
+// Status messages
 const FOUND_MESSAGE = "Profile Readme has been hidden.";
 const NOT_FOUND_MESSAGE = "No profile readme found on this page.";
 
-
+// After DOM has been loaded, send a status request to the content script
 document.addEventListener("DOMContentLoaded", function () {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {message: "status"}, function(response) {
             if (!chrome.runtime.lastError && response !== undefined) {
+                // Set status text of the popup
                 let statusText = response.found === true ? FOUND_MESSAGE : NOT_FOUND_MESSAGE;
                 document.getElementById("status-text").innerHTML = statusText;
             }
